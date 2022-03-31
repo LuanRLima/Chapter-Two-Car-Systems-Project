dcdca146cc77ce39bd8944685e73b801bcd03836
Luan Rodrigues Lima <luanrlima9@gmail.com>
2022-03-31 06:07:29 -0300

    Creating services, repository, routes and model using the Single Responsibility Principle(SRP)

---
+++ src/model/Category.ts
@@ -0,0 +1,17 @@
+import { v4 as uuidv4 } from 'uuid';
+
+class Category {
+    id?: string
+    name: string;
+    description: string;
+    createed_at: Date;
+
+    constructor() {
+        if(!this.id){
+            this.id = uuidv4();
+        }
+        
+    }
+}
+
+export { Category };
