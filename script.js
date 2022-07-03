2c1c1a7119e125e8a64e6fdbece79c2a526c0ee3
Luan Rodrigues Lima <luanrlima9@gmail.com>
2022-07-02 23:26:54 -0300

    refatoração

---
+++ script.js
@@ -0,0 +1,36 @@
+{
+    financialRules = {
+        "dailyLimitCurrency": 7000,
+        "limitePerTransaction": 1000,
+        "monthlyLimitCurrency": 20000,
+        "quantityOfTransaction": 100,
+        "currencySupportedForSettlement": [840]
+    };
+
+    inquiry = {
+        "visaNetworkInfo": [
+            {
+                "cardProductType": "D",
+                "billingCurrency": 840,
+                "billingCurrencyCodeMinorDigits": "2",
+                "issuerName": "Visa Test Bank",
+                "nonMoneyTransferPushFundsDomestic": "N",
+                "nonMoneyTransferPushFundsCrossBorder": "N",
+                "onlineGamblingPushFundsDomestic": "Y",
+                "onlineGamblingPushFundsCrossBorder": "Y",
+                "moneyTransferPushFundsDomestic": "N",
+                "moneyTransferPushFundsCrossBorder": "N",
+                "nonMoneyTransferFastFundsDomestic": "N",
+                "nonMoneyTransferFastFundsCrossBorder": "Y",
+                "onlineGamblingFastFundsDomestic": "Y",
+                "onlineGamblingFastFundsCrossBorder": "Y",
+                "moneyTransferFastFundsDomestic": "N",
+                "moneyTransferFastFundsCrossBorder": "Y",
+                "comboCardRange": "0",
+                "cardPlatformCode": "CN",
+                "cardSubTypeCode": "-",
+                "issuerCountryCode": 840
+            }
+        ]
+    }
+}
