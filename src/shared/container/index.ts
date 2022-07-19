d0a405998c149a8590d81cd251c3c50e67e38680
Luan Rodrigues Lima <luanrlima9@gmail.com>
2022-07-19 09:17:41 -0300

    creating the singleton container of UserRepository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,11 @@
 import { container } from 'tsyringe';
 
+import { UsersRepository } from '../../modules/cars/accounts/repositories/implementations/UsersRepository';
+import { IUsersRepository } from '../../modules/cars/accounts/repositories/IUsersRepository';
+
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+
 import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
 
@@ -9,3 +13,5 @@ import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpec
 container.registerSingleton<ICategoriesRepository>('CategoriesRepository', CategoriesRepository);
 
 container.registerSingleton<ISpecificationsRepository>('SpecificationsRepository', SpecificationsRepository);
+
+container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository)
